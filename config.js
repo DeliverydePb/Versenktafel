a29e0411c0382285488d29d361ab558c0543020b
DeliverydePb <156477707+DeliverydePb@users.noreply.github.com>
2026-05-09 01:48:21 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Archivo de Configuración - No tocar la estructura, solo los valores
+const CONFIG = {
+    URL_API: "https://script.google.com/macros/s/AKfycbyKyUJ4WTUVaIs_O8gx8EtO8pn25MqcdH8omrFzfoxSMZakBOWcoDEbJiAL3FAtUL31Aw/exec",
+    CLIENT_ID: "653070334239-trc9c573n3e9ji9iu6j4b23ueh7bok9l.apps.googleusercontent.com"
+};
