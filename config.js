ba82ec36c0172bbfc9ddedb85593238818cef4b8
DeliverydePb <156477707+DeliverydePb@users.noreply.github.com>
2026-05-15 07:35:15 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Configuración del proyecto
+const CONFIG = {
+    // Reemplaza esto con la URL que te dará Google Apps Script
+    API_URL: "https://script.google.com/macros/s/AKfycbyKyUJ4WTUVaIs_O8gx8EtO8pn25MqcdH8omrFzfoxSMZakBOWcoDEbJiAL3FAtUL31Aw/exec"
+};
