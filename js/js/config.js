24b1c1ae73670ab893780859a1d371f4f219c440
DeliverydePb <156477707+DeliverydePb@users.noreply.github.com>
2026-06-05 17:59:09 -0300

    Delete js/js directory

--- js/js/config.js
+++
@@ -1,5 +0,0 @@
-// js/config.js
-const CONFIG = {
-    GOOGLE_CLIENT_ID: "653070334239-trc9c573n3e9ji9iu6j4b23ueh7bok9l.apps.googleusercontent.com",
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwn9fxjENxBVL7wCS7tay-vzjExZbYzCMe3otHN7GL7CpJjxfCTVqYJa8zjL9BTdG_hsQ/exec"
-};
