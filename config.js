14e31e83927c8c3ec0fe647274f0bebe04bac0be
DeliverydePb <156477707+DeliverydePb@users.noreply.github.com>
2026-06-05 12:29:05 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ const CONFIG = {
     GOOGLE_CLIENT_ID: "653070334239-trc9c573n3e9ji9iu6j4b23ueh7bok9l.apps.googleusercontent.com",
     
     // Dejamos esto listo para cuando configuremos el puente con Google Sheets
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzdGfX1xGpVUiRgpg1RClyIouzvY0oXeez7h_1LoQF9nyjyMU1qX4LB3qU9KZg-SUbEZg/exec",
-    
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwn9fxjENxBVL7wCS7tay-vzjExZbYzCMe3otHN7GL7CpJjxfCTVqYJa8zjL9BTdG_hsQ/exec",
     // Opciones de configuración del juego (por si luego quieres cambiar nombres o valores)
     GAME_SETTINGS: {
         START_YEAR: 1939,
