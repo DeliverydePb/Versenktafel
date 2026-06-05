6c22e6e2dce0768eb7bf969adb7b819b2b0582c2
DeliverydePb <156477707+DeliverydePb@users.noreply.github.com>
2026-06-05 20:54:32 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// CONFIGURACIÓN GLOBAL DE APIS (Centralización de IDs estáticos)
+const CONFIG = {
+    GOOGLE_CLIENT_ID: "653070334239-trc9c573n3e9ji9iu6j4b23ueh7bok9l.apps.googleusercontent.com",
+    SPREADSHEET_ID: "1-n-FtiUbmh74Eaco_Si6N8qfmASmpuZ-nwPI_PR2N84",
+    GOOGLE_API_KEY: "AIzaSyDeTt8PSBWGOQzfxWRhx88dm0Yn-GvpEE0"
+};
