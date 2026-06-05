ea0179f9d495f02cb3ac74b6e861ab414dba80d5
DeliverydePb <156477707+DeliverydePb@users.noreply.github.com>
2026-06-05 17:50:09 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,15 +1,5 @@
-// config.js
+// js/config.js
 const CONFIG = {
     GOOGLE_CLIENT_ID: "653070334239-trc9c573n3e9ji9iu6j4b23ueh7bok9l.apps.googleusercontent.com",
-    
-    // Dejamos esto listo para cuando configuremos el puente con Google Sheets
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwn9fxjENxBVL7wCS7tay-vzjExZbYzCMe3otHN7GL7CpJjxfCTVqYJa8zjL9BTdG_hsQ/exec",
-    // Opciones de configuración del juego (por si luego quieres cambiar nombres o valores)
-    GAME_SETTINGS: {
-        START_YEAR: 1939,
-        DEFAULT_PORT: "Kiel"
-    }
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwn9fxjENxBVL7wCS7tay-vzjExZbYzCMe3otHN7GL7CpJjxfCTVqYJa8zjL9BTdG_hsQ/exec"
 };
-
-// Congelamos el objeto para evitar modificaciones accidentales en tiempo de ejecución
-Object.freeze(CONFIG);
